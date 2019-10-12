163eabd6c81c4065082699a8d6fc0c050955da3f
INDEZENDE <i.rios.zendejas@gmail.com>
2019-10-12 13:21:35 -0500

    Creando Repo

---
+++ AutoLAyout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLAyout
+//
+//  Created by Apple Device 13 on 10/12/19.
+//  Copyright © 2019 Inde Rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
